417cea4abe1a9c2f9608589c15168c220a860f44
UrbanPistek <urban.pistek@gmail.com>
2025-12-21 09:54:12 -0700

    test build

--- inject_ENV.sh
+++ inject_ENV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Inject API key into html before build
-sed -i "s|__MAP_API_KEY__|${GOOGLE_MAPS_API_KEY}|g" ../../index.html
+sed -i "s|__MAP_API_KEY__|${GOOGLE_MAPS_API_KEY}|g" index.html
 echo "Environment variables injected successfully"
