e80baa9a0775cee01744a8e1e64833301e32d633
UrbanPistek <urban.pistek@gmail.com>
2025-12-21 10:32:12 -0700

    Trying to get variable injection to work

--- inject_ENV.sh
+++ inject_ENV.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+pwd
+echo "Check test key value: ${INJECTION_ENV_TEST_KEY}"
+
 # Inject API key into html before build
 sed -i "s|__MAP_API_KEY__|${GOOGLE_MAPS_API_KEY}|g" index.html
 sed -i "s|__INJECTION_ENV_TEST_KEY__|${INJECTION_ENV_TEST_KEY}|g" index.html
